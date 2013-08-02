f7b2c4888aa2c042aa781ea49abd18e4e8d2364f
pikipity <wangze19910407@gmail.com>
2013-08-02 18:19:33 +0800

    基本完成，仍需改进
    IMF判定仍然不明确
    计算耗时长

---
+++ EMD.m
@@ -0,0 +1,52 @@
+function [C R] = EMD( s, step, thread )
+% Calculate the EMD of s
+%
+% [C R] = EMD( s, step, thread )
+%
+%Input:
+%   s: input signal
+%   step: maximum step
+%   thread: use to determine IMF
+%Output:
+%   C are IMF components
+%   R=s-c
+
+IsIMF=0;
+IsMonotonic=0;
+L=length(s);
+t=1:L;
+n=0;
+C=[];
+R=[];
+h=s;
+while IsMonotonic==0
+    n=n+1;
+    while IsIMF==0
+        [MaxPosition MaxValue NumMax MinPosition MinValue NumMin]=LocalMaxMin(h);
+        pp=csape(MaxPosition,MaxValue,'variational');
+        UpEnvelope=ppval(pp,t);
+        pp=csape(MinPosition,MinValue,'variational');
+        DownEnvelope=ppval(pp,t);
+        m=(UpEnvelope+DownEnvelope)./2;
+        h=h-m;
+        [MaxPosition MaxValue NumMax MinPosition MinValue NumMin]=LocalMaxMin(h);
+        pp=csape(MaxPosition,MaxValue,'variational');
+        UpEnvelope=ppval(pp,t);
+        pp=csape(MinPosition,MinValue,'variational');
+        DownEnvelope=ppval(pp,t);
+        if NumMax(2)+NumMin(2)<=2 && NumMax(3)==0 && NumMin(1)==0 && ~any(((UpEnvelope+DownEnvelope)./2)>thread) && ~any(((UpEnvelope+DownEnvelope)./2)<-thread)
+            IsIMF=1;
+        end
+    end
+    s=s-h;
+    R(n,:)=s;
+    C(n,:)=h;
+    [MaxPosition MaxValue NumMax MinPosition MinValue NumMin]=LocalMaxMin(s);
+    if (isempty(MaxValue) && isempty(MinValue)) || n>=step
+        IsMonotonic=1;
+    end
+    h=s;
+end
+            
+end
+
--- LocalMaxMin.m
+++ LocalMaxMin.m
@@ -1,10 +1,19 @@
-function [ MaxPosition LocalMaximum MinPosition LocalMinimum ] = LocalMaxMin( x )
+function [ MaxPosition LocalMaximum NumMax MinPosition LocalMinimum NumMin ] = LocalMaxMin( x )
 %   find the Local Maximum and Local Minimum values and their position in x
 %which is a one dimention array
 %
-%[ LocalMaximum MaxPosition LocalMinimum MinPosition ] = LocalMaxMin( x )
+% [ MaxPosition LocalMaximum NumMax MinPosition LocalMinimum NumMin ] = LocalMaxMin( x )
 %
 %Note: the first and the final values (not positions) are lost in results.
+%The continuous same values will be counted as different points when
+%calculate the "MaxPosition"/"MinPosition" and
+%"LocalMaximum"/"LocalMinimum". But they will be counted as the same point
+%when calculate the "NumMax" and the "NumMin".
+%
+%   In the "NumMax" and "NumMin", the first value is the number of points
+%   which are larger than 0. The second value is the number of points which
+%   are equal to 0. The theird value is the number of points which are
+%   smaller than 0.
 
 xSize=size(x);
 if xSize(1)~=1 || xSize(2)<4
@@ -15,6 +24,8 @@ LocalMaximum=[];
 MaxPosition=[];
 LocalMinimum=[];
 MinPosition=[];
+NumMax=[0 0 0];
+NumMin=[0 0 0];
 
 StartPosition=0;
 
@@ -28,9 +39,23 @@ for AdjustPosition=Position
         if d1(AdjustPosition)<0
             LocalMinimum=[LocalMinimum x(AdjustPosition+1)];
             MinPosition=[MinPosition AdjustPosition+1];
+            if x(AdjustPosition+1)>0
+                NumMin(1)=NumMin(1)+1;
+            elseif x(AdjustPosition+1)==0
+                NumMin(2)=NumMin(2)+1;
+            else
+                NumMin(3)=NumMin(3)+1;
+            end
         elseif d1(AdjustPosition)>0
             LocalMaximum=[LocalMaximum x(AdjustPosition+1)];
             MaxPosition=[MaxPosition AdjustPosition+1];
+            if x(AdjustPosition+1)>0
+                NumMax(1)=NumMax(1)+1;
+            elseif x(AdjustPosition+1)==0
+                NumMax(2)=NumMax(2)+1;
+            else
+                NumMax(3)=NumMax(3)+1;
+            end
         end
     else
         if d1(AdjustPosition)==0 && d2(AdjustPosition)~=0
@@ -38,18 +63,46 @@ for AdjustPosition=Position
                 if d2(AdjustPosition)>0
                     LocalMinimum=[LocalMinimum x(2:AdjustPosition+1)];
                     MinPosition=[MinPosition (2:AdjustPosition+1)];
+                    if x(AdjustPosition+1)>0
+                        NumMin(1)=NumMin(1)+1;
+                    elseif x(AdjustPosition+1)==0
+                        NumMin(2)=NumMin(2)+1;
+                    else
+                        NumMin(3)=NumMin(3)+1;
+                    end
                 else
                     LocalMaximum=[LocalMaximum x(2:AdjustPosition+1)];
                     MaxPosition=[MaxPosition (2:AdjustPosition+1)];
+                     if x(AdjustPosition+1)>0
+                        NumMax(1)=NumMax(1)+1;
+                    elseif x(AdjustPosition+1)==0
+                        NumMax(2)=NumMax(2)+1;
+                     else
+                        NumMax(3)=NumMax(3)+1;
+                    end
                 end
             else
                 if d1(StartPosition)*d2(AdjustPosition)<0
                     if d2(AdjustPosition)>0
                         LocalMinimum=[LocalMinimum x(StartPosition+1:AdjustPosition+1)];
                         MinPosition=[MinPosition (StartPosition+1:AdjustPosition+1)];
+                        if x(AdjustPosition+1)>0
+                            NumMin(1)=NumMin(1)+1;
+                        elseif x(AdjustPosition+1)==0
+                            NumMin(2)=NumMin(2)+1;
+                        else
+                            NumMin(3)=NumMin(3)+1;
+                        end
                     else
                         LocalMaximum=[LocalMaximum x(StartPosition+1:AdjustPosition+1)];
                         MaxPosition=[MaxPosition (StartPosition+1:AdjustPosition+1)];
+                        if x(AdjustPosition+1)>0
+                            NumMax(1)=NumMax(1)+1;
+                        elseif x(AdjustPosition+1)==0
+                            NumMax(2)=NumMax(2)+1;
+                        else
+                            NumMax(3)=NumMax(3)+1;
+                        end
                     end
                 end
             end
@@ -63,9 +116,23 @@ if StartPosition~=0
     if d1(StartPosition)<0
                 LocalMinimum=[LocalMinimum x(StartPosition+1:length(x)-1)];
                 MinPosition=[MinPosition (StartPosition+1:length(x)-1)];
+                if x(length(x)-1)>0
+                       NumMin(1)=NumMin(1)+1;
+                elseif x(length(x)-1)==0
+                        NumMin(2)=NumMin(2)+1;
+                 else
+                        NumMin(3)=NumMin(3)+1;
+                 end
     else
                 LocalMaximum=[LocalMaximum x(StartPosition+1:length(x)-1)];
                 MaxPosition=[MaxPosition (StartPosition+1:length(x)-1)];
+                if x(length(x)-1)>0
+                            NumMax(1)=NumMax(1)+1;
+                 elseif x(length(x)-1)==0
+                            NumMax(2)=NumMax(2)+1;
+                 else
+                            NumMax(3)=NumMax(3)+1;
+                 end
     end
 end
 end
